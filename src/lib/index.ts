da4d7806c86a60f543ba5a771e65e51904fe97df
Aleksei Epifantsev <alex@lahtihosting.fi>
2023-04-01 16:52:14 +0400

    feat: package version 0.2.0

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -15,5 +15,6 @@ export { default as FieldEmail } from './components/form/FieldEmail.svelte';
 export { default as FieldTextarea } from './components/form/FieldTextarea.svelte';
 export { default as FieldCheckbox } from './components/form/FieldCheckbox.svelte';
 export { default as FieldPassword } from './components/form/FieldPassword.svelte';
+export { default as FieldSearch } from './components/form/FieldSearch.svelte';
 export { default as FieldSelect } from './components/form/FieldSelect.svelte';
 export type { FieldSelectItem as FieldSelectItem } from './components/form/FieldSelect.svelte';
